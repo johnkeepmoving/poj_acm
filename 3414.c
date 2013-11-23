8309ae2a710de2787e9ef94908316ff823381f63
jiangheng <jiangheng@debian>
2013-11-23 12:11:27 +0800

    .

--- 3414.c
+++ 3414.c
@@ -58,14 +58,19 @@ int empty()
         return 0;
 }
 
+void set_node(node *p, int v1, int v2, int step, int action, int pre)
+{
+    p -> v1 = v1;
+    p -> v2 = v2;
+    p -> step = step;
+    p -> action = action;
+    p -> pre = pre;
+}
+
 int get_ans()
 {
     node *curr_node = (node *)malloc(sizeof(node));
-    curr_node -> v1 = 0;
-    curr_node -> v2 = 0;
-    curr_node -> step = 0;
-    curr_node -> action = 0;
-    curr_node -> pre = -1;
+    set_node( curr_node, 0, 0, 0, 0, -1);
     Enq( curr_node );
     flags[0][0] = 1;
     while( empty() == 0)
@@ -73,78 +78,51 @@ int get_ans()
         node *t_node;
         t_node = Deq();
         int curr_index = head -1;
-        printf("this pop: v1 is %d, v2 is %d.Its step is %d\n", t_node -> v1, t_node -> v2, t_node->step);
         if( t_node -> v1 == C || t_node -> v2 == C )
         {
             return head-1;
         }
         node *next_node = (node *)malloc(sizeof(node));
         copy_node(t_node, curr_node);
-        copy_node(curr_node, next_node);
+        copy_node(t_node, next_node);
         if( (curr_node -> v1 < A) && (flags[A][curr_node->v2] != 1) )
         {
-            next_node -> v1 = A;
-            next_node -> step = curr_node -> step + 1;
-            next_node -> action = 1;
-            next_node -> pre = curr_index;
+            set_node(next_node, A, curr_node->v2, curr_node->step + 1, 1, curr_index);
             Enq( next_node );
-            printf("add node %d %d\n", A, curr_node->v2);
-            flags[A][curr_node->v2] = 1;
+            flags[next_node->v1][next_node->v2] = 1;
         }
         if( (curr_node -> v1 > 0) && (flags[0][curr_node->v2] != 1) )
         {
-            next_node -> v1 = 0;
-            next_node -> step = curr_node -> step + 1;
-            next_node -> action = 3;
-            next_node -> pre = curr_index;
+            set_node(next_node, 0, curr_node->v2, curr_node->step + 1, 3, curr_index);
             Enq( next_node );
-            printf("add node %d %d\n", 0, curr_node->v2);
-            flags[0][curr_node->v2] = 1;
+            flags[next_node->v1][next_node->v2] = 1;
         }
         if( (curr_node -> v2 < B) && (flags[curr_node->v1][B] != 1) )
         {
-            next_node -> v2 = B;
-            next_node -> step = curr_node -> step + 1;
-            next_node -> action = 2;
-            next_node -> pre = curr_index;
+            set_node(next_node, curr_node->v1, B, curr_node->step + 1, 2, curr_index);
             Enq( next_node );
-            printf("add node %d %d\n", curr_node->v1, B);
-            flags[curr_node->v1][B] = 1;
+            flags[next_node->v1][next_node->v2] = 1;
         }
         if( (curr_node -> v2 > 0) && (flags[curr_node->v1][0] != 1) )
         {
-            next_node -> v2 = 0;
-            next_node -> step = curr_node -> step + 1;
-            next_node -> action = 4;
-            next_node -> pre = curr_index;
+            set_node(next_node, curr_node->v1, 0, curr_node->step + 1, 4, curr_index);
             Enq( next_node );
-            printf("add node %d %d\n", curr_node->v1, 0);
-            flags[curr_node->v1][0] = 1;
+            flags[next_node->v1][next_node->v2] = 1;
         }
         if( (curr_node -> v1 > 0) && ( curr_node->v2 < B) )
         {
             int diff = curr_node->v1 - (B - curr_node->v2);
             if( (diff >= 0) && (flags[diff][B] != 1) )
             {
-                next_node -> v1 = diff;
-                next_node -> v2 = B;
-                next_node -> step = curr_node -> step + 1;
-                next_node -> action = 5;
-                next_node -> pre = curr_index;
+                set_node(next_node, diff, B, curr_node->step + 1, 5, curr_index);
                 Enq( next_node );
-                printf("add node %d %d\n", diff, B);
-                flags[diff][B] = 1;
+                flags[next_node->v1][next_node->v2] = 1;
             }
             if( (diff < 0) && (flags[0][B+diff] != 1) )
             {
-                next_node -> v1 = 0;
-                next_node -> v2 = B+diff;
-                next_node -> step = curr_node -> step + 1;
-                next_node -> action = 5;
-                next_node -> pre = curr_index;
+                set_node(next_node, 0, B+diff, curr_node->step + 1, 5, curr_index);
                 Enq( next_node );
-                printf("add node %d %d\n", 0, B+diff);
-                flags[0][B+diff] = 1;
+                flags[next_node->v1][next_node->v2] = 1;
             }
         }
         if( (curr_node -> v2 > 0) && ( curr_node->v1 < A) )
@@ -152,28 +130,19 @@ int get_ans()
             int diff = curr_node->v2 - (A - curr_node->v1);
             if( (diff >= 0) && (flags[A][diff] != 1) )
             {
-                next_node -> v1 = A;
-                next_node -> v2 = diff;
-                next_node -> step = curr_node -> step + 1;
-                next_node -> action = 6;
-                next_node -> pre = curr_index;
+                set_node(next_node, A, diff, curr_node->step + 1, 6, curr_index);
                 Enq( next_node );
-                printf("add node %d %d\n", A,diff);
-                flags[A][diff] = 1;
+                flags[next_node->v1][next_node->v2] = 1;
             }
             if( (diff < 0) && (flags[A+diff][0] != 1) )
             {
-                next_node -> v1 = A+diff;
-                next_node -> v2 = 0;
-                next_node -> step = curr_node -> step + 1;
-                next_node -> action = 6;
-                next_node -> pre = curr_index;
+                set_node(next_node, A+diff, 0, curr_node->step + 1, 6, curr_index);
                 Enq( next_node );
-                printf("add node %d %d\n", A+diff, 0);
-                flags[A+diff][0] = 1;
+                flags[next_node->v1][next_node->v2] = 1;
             }
         }
     }
+    return -1;
 }
 
 void print_ans(int index)
@@ -185,9 +154,13 @@ void print_ans(int index)
 int main()
 {
     scanf("%d%d%d", &A, &B, &C);
-    printf("A is %d, B is %d, C is %d\n", A, B, C);
     head = 0; tail = 0;
     int last_index = get_ans();
+    if( last_index == -1 )
+    {
+        printf("impossible\n");
+        return 1;
+    }
     int step = que[last_index].step;
     printf("%d\n", step);
     print_ans(last_index);

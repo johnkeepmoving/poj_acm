c30ab18b6ba21c182e0cb1c86e67a1e579364897
jiangheng <johnloveryan@gmail.com>
2014-01-11 11:52:09 +0800

    simple greedy

--- baseAlgorithms/greedy/2586.cpp
+++ baseAlgorithms/greedy/2586.cpp
@@ -1,7 +1,26 @@
 #include<cstdio>
-
 using namespace std;
+
+int s, d;
+int totalSurplus;
 int main()
 {
+    while( scanf("%d%d", &s, &d) != -1 )
+    {
+        if ( d > 4*s )
+            totalSurplus = 10*s - 2*d;
+        else if ( d > 1.5*s )
+            totalSurplus = 8*s - 4*d;
+        else if ( d > 2.0/3.0*s )
+            totalSurplus = 6*s - 6*d;
+        else if ( d > 1.0/4.0*s )
+            totalSurplus = 3*s - 9*d;
+        else
+            totalSurplus = -12*d;
+        if( totalSurplus > 0 )
+            printf("%d\n", totalSurplus);
+        else
+            printf("Deficit\n");
+    }
     return 0;
 }
